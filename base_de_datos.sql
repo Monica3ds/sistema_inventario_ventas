e64708dbc7c827fb3a3366cda9d44fb096ab6f01
Monica Liliana Membreño Rivas <6107859@incb.edu.sv>
2026-09-18 16:25:52 -0600

    Agrega tabla y vista de proveedores

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -72,3 +72,4 @@ SELECT c.nombre_categoria, SUM(p.stock) AS existencias_totales
 FROM productos p
 INNER JOIN categorias c ON p.categoria_id = c.id
 GROUP BY c.nombre_categoria;
+CREATE TABLE e INSERT
